9d81bb139bcf1811ea31dd968de22f2df0a88e34
unknown <konevoliskolujos@gmail.com>
2020-04-16 13:20:26 +0200

    initial commit for publishment

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\PC\Desktop\twitter_clone_publish\twitter_clone\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
